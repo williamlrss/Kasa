be2880a1dbe8ac6fd679bf070b52ef450331cef1
William Lerossignol <williamlerossignol@outlook.com>
2023-07-25 14:55:40 +0100

    Ready for build

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './navbar.scss';
 
 function Navbar() {
 	const location = useLocation();
-	const [activeLink, setActiveLink] = useState(() => (location.pathname === '/' || location.pathname === '/kasa' ? '/kasa' : location.pathname));
 
 	const navLinks = [
 		{ name: 'Home', path: '/kasa' },
 		{ name: 'À propos', path: '/about' },
 	];
 
-	useEffect(() => {
-		setActiveLink(location.pathname === '/' || location.pathname === '/kasa' ? '/kasa' : location.pathname);
-	}, [location]);
-
 	return (
 		<nav>
 			<ul className='nav-list'>
 				{navLinks.map((link) => (
 					<li key={link.path}>
-						<Link to={link.path} className={`nav-list__item ${activeLink === link.path ? 'nav-list__item--active' : ''}`}>
+						<Link
+							to={link.path}
+							className={`nav-list__item ${
+								location.pathname === link.path || (location.pathname === '/' && link.path === '/kasa') ? 'nav-list__item--active' : ''
+							}`}
+						>
 							{link.name}
 						</Link>
 					</li>
